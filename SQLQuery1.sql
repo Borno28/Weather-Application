7bdc4e8ff1bbabcde656d1a6b4ae37de22e6505b
Md Adlul Islam <118211648+Borno28@users.noreply.github.com>
2024-05-13 22:45:48 +0600

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿create table admin1
+(
+		id int primary key identity(1,1),
+		email varchar(max) null,
+		username varchar(max) null,
+		passowrd varchar(max) null,
+		date_created date null
+
+)
+select * from admin1
